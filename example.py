13002ed88f1da481911c54a2da581afed2696bca
Brody Tedesco <brody.dom.tedesco@gmail.com>
2022-06-06 17:57:59 +1000

    GUI Update

--- example.py
+++ example.py
@@ -5,8 +5,9 @@ class Demo1:
         self.master = master
         self.master.geometry("400x400")
         self.frame = tk.Frame(self.master)
-        self.butnew("Window 1", "ONE", Demo2)
-        self.butnew("Window 2", "TWO", Demo3)
+        self.butnew("SingleSpice", "ONE", SingleSpice)
+        self.butnew("Recipes", "TWO", Recipes)
+        self.butnew("Recipes", "TWO", Recipes)
         self.frame.pack()
 
     def butnew(self, text, number, _class):
@@ -17,13 +18,50 @@ class Demo1:
         _class(self.newWindow, number)
 
 
-class Demo2:
+class SingleSpice:
+    def __init__(self, master, number):
+        self.master = master
+        self.master.geometry("400x400+400+400")
+        self.frame = tk.Frame(self.master)
+        
+        self.quitButton = tk.Button(self.frame, text = 'Back', width = 25, command = self.close_windows)
+        
+        # self.butnew("Recipes1", "a" , SingleSpiceDispenser)
+        # self.butnew("Recipes1", "a" , SingleSpiceDispenser)
+        # self.butnew("Recipes1", "a" , SingleSpiceDispenser)
+          
+
+        self.butnew("Recipes", "TWO", Recipes)
+
+        self.label = tk.Label(master, text=f"this is window number {number}")
+
+
+
+        self.label.pack()
+        self.quitButton.pack()
+
+       
+
+        self.frame.pack()
+
+    def close_windows(self):
+        self.master.destroy()
+
+    def butnew(self, text, number, _class):
+        tk.Button(self.frame, text, width = 25, command = lambda: self.new_window(number, _class)).pack()
+
+    def new_window(self, number, _class):
+        self.newWindow = tk.Toplevel(self.master)
+        _class(self.newWindow, number)
+        
+
+class SingleSpiceDispenser:
     def __init__(self, master, number):
         self.master = master
         self.master.geometry("400x400+400+400")
         self.frame = tk.Frame(self.master)
         self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
-        self.label = tk.Label(master, text=f"this is window number {number}")
+        self.label = tk.Label(master, text=f"this is window number {spices[number]}")
         self.label.pack()
         self.quitButton.pack()
         self.frame.pack()
@@ -31,12 +69,31 @@ class Demo2:
     def close_windows(self):
         self.master.destroy()
 
-class Demo3:
+    def butnew(self, text, number, _class):
+        tk.Button(self.frame, text = text, width = 25, command = lambda: self.new_window(number, _class)).pack()
+
+    def new_window(self, number, _class):
+        self.newWindow = tk.Toplevel(self.master)
+        _class(self.newWindow, number)
+
+
+
+    def close_windows(self):
+        self.master.destroy()
+
+    def butnew(self, text, number, _class):
+        tk.Button(self.frame, text = text, width = 25, command = lambda: self.new_window(number, _class)).pack()
+
+    def new_window(self, number, _class):
+        self.newWindow = tk.Toplevel(self.master)
+        _class(self.newWindow, number)
+
+class Recipes:
     def __init__(self, master, number):
         self.master = master
         self.master.geometry("400x400+400+400")
         self.frame = tk.Frame(self.master)
-        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.quitButton = tk.Button(self.frame, text = 'Back', width = 25, command = self.close_windows)
         self.label = tk.Label(master, text=f"this is window number {number}")
         self.label.pack()
         self.label2 = tk.Label(master, text="THIS IS HERE TO DIFFERENTIATE THIS WINDOW")
@@ -48,12 +105,14 @@ class Demo3:
         self.master.destroy()
 
 
-
+spices = {0:"salt", 1:"pepper", 2:"paprika", 3:"tumeric"}
 
 def main(): 
     root = tk.Tk()
     app = Demo1(root)
     root.mainloop()
+
+
 
 if __name__ == '__main__':
     main()
